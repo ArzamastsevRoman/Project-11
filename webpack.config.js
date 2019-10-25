831c361007b01079b89734826f997221a0922bf7
Roman Arzamastsev <arzamastsevroma@yandex.ru>
2019-10-25 17:27:48 +0300

    updated

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
 const webpack = require('webpack');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin'); 
-//const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
